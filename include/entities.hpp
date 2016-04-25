53800e07513f4a625ba70ff7d805d158a6b42121
drumsetmonkey <abelleisle@roadrunner.com>
2016-04-24 22:53:43 -0400

    Stuff

--- include/entities.hpp
+++ include/entities.hpp
@@ -68,6 +68,7 @@ enum BUILD_SUB{
 	LAMP_POST,			/**< a lamppost, creates light */
 	FIRE_PIT,			/**< a firepit, creates fire particles / light */
 	STALL_MARKET = 70,	/**< a stall for a merchant */
+	STALL_TRADER
 };
 
 /**
