ddac347ea12637bdc63a1b126a393d6e35313204
Clyne Sullivan <tullivan99@gmail.com>
2016-11-29 20:51:12 -0500

    build script

--- build.sh
+++ build.sh
@@ -3,10 +3,12 @@
 idate=$(date +"%s" -r .git/index)
 odate=$(date +"%s" -r ./.updated)
 
-echo $idate
-echo $odate
-
 if (( $idate > $odate )); then
-	echo "frig"
+	echo "Project updated, building all..."
+	make clean
+	make -j4
 	touch ./.updated
+else
+	echo "Up to date, building..."
+	make -j4
 fi
